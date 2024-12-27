2b1c771229f65206ce54980404c9bccbe7a70848
diogo varaschin <diogovaraschin@outlook.com>
2024-12-27 15:24:58 -0300

    class 2 completed

---
+++ aula2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>variaveis em php</title>
+    </head>
+    <body>
+        <?php 
+            $idade=21;
+            $nome="Diogo Varaschin";
+            echo "$nome tem $idade anos!"
+        ?>
+    </body>
+</html>
