d154100fadc8820a48667c50d9a11e382e4e014f
diogo varaschin <diogovaraschin@outlook.com>
2024-12-27 12:00:07 -0300

    final version of the aula1

--- aula1/index.php
+++ aula1/index.php
@@ -3,13 +3,20 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>primeiro ex de php</title>
+        <style>
+            h2 {
+                color: pink;
+                text-shadow: 1px 1px 2px black;
+            }
+        </style>
     </head>
     <body>
         <h1>testando o php</h1>
         <?php 
             echo "ola mundo!";
             echo "<h2>ola mundo</h2>";
+            echo "<h2>ola <br> mundo</h2>";
         ?>
     </body>
 </html>
