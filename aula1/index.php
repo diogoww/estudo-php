cc04f3a225fe888559562d257fa294ace62d39e0
diogo varaschin <diogovaraschin@outlook.com>
2024-12-27 11:49:18 -0300

    starting point of the study

---
+++ aula1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>testando o php</h1>
+        <?php 
+            echo "ola mundo!";
+            echo "<h2>ola mundo</h2>";
+        ?>
+    </body>
+</html>
